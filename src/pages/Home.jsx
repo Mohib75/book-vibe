add0783dd6b2dafe56d84f3c8295652e8c91f270
Ifthakher Rahman Mohib <ifthakherrahman426@gmail.com>
2024-03-28 10:57:08 +0600

    home page updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,14 @@
+import { useLoaderData } from "react-router-dom"
+import Books from "../components/Books"
+import Hero from "../components/Hero"
+
 const Home = () => {
+	const books = useLoaderData()
+	// console.log(books)
 	return (
-		<div className='h-screen w-full flex flex-col items-center justify-center'>
-			<h1>Home</h1>
+		<div className='mt-8 mx-4'>
+			<Hero />
+			<Books books={books} />
 		</div>
 	)
 }
