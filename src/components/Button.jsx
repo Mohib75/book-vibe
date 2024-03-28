274ece5769c01c2b29727efa1485cd7322f1a2b0
Ifthakher Rahman Mohib <ifthakherrahman426@gmail.com>
2024-03-28 10:54:49 +0600

    button added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,11 @@
 import PropTypes from "prop-types"
 
-const Button = ({ className, text }) => {
-	return <a className={className}>{text}</a>
+const Button = ({ className, text, onClick }) => {
+	return (
+		<button onClick={onClick} className={className}>
+			{text}
+		</button>
+	)
 }
 
 export default Button
@@ -9,4 +13,5 @@ export default Button
 Button.propTypes = {
 	className: PropTypes.string,
 	text: PropTypes.string,
+	onClick: PropTypes.func,
 }
