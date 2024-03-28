1ef8cecf2000f79c9bd53988e421f430161700da
Ifthakher Rahman Mohib <ifthakherrahman426@gmail.com>
2024-03-28 10:56:00 +0600

    routes updated

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,19 +2,60 @@ import { createBrowserRouter } from "react-router-dom"
 import MainLayout from "../layout/MainLayout"
 import Home from "../pages/Home"
 import ListedBooks from "../pages/ListedBooks"
+import PagesToRead from "../pages/PagesToRead"
+import BookDetail from "../pages/BookDetail"
+import ReadBook from "../components/ReadBook"
+import WishlistBook from "../components/WishlistBook"
+import ErrorPage from "../components/ErrorPage"
+import Contact from "../pages/Contact"
+import AllBooks from "../pages/AllBooks"
 
 export const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <MainLayout />,
+		errorElement: <ErrorPage />,
 		children: [
 			{
 				path: "/",
+				loader: () => fetch("/book.json"),
 				element: <Home />,
 			},
 			{
 				path: "/listedBooks",
+				loader: () => fetch("/book.json"),
 				element: <ListedBooks />,
+				children: [
+					{
+						index: true,
+						element: <ReadBook />,
+						loader: () => fetch("/book.json"),
+					},
+					{
+						path: "wishlist",
+						element: <WishlistBook />,
+						loader: () => fetch("/book.json"),
+					},
+				],
+			},
+			{
+				path: "/pagesToRead",
+				loader: () => fetch("/book.json"),
+				element: <PagesToRead />,
+			},
+			{
+				path: "/bookDetail/:id",
+				loader: () => fetch("/book.json"),
+				element: <BookDetail />,
+			},
+			{
+				path: "/allBooks",
+				loader: () => fetch("/book.json"),
+				element: <AllBooks />,
+			},
+			{
+				path: "/contact",
+				element: <Contact />,
 			},
 		],
 	},
