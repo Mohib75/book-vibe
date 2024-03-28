c4bc15058b486795685773e8670243d895f94d1e
Ifthakher Rahman Mohib <ifthakherrahman426@gmail.com>
2024-03-28 10:54:11 +0600

    main js updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom/client"
 import "./index.css"
 import { RouterProvider } from "react-router-dom"
 import { router } from "./routes/Routes.jsx"
+import { Toaster } from "react-hot-toast"
+import { FilterProvider } from "./utils/filterContext.jsx"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<FilterProvider>
+			<RouterProvider router={router} />
+			<Toaster />
+		</FilterProvider>
 	</React.StrictMode>
 )
